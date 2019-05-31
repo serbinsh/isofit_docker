f97558dd89cc58dc33fc573f8a1a46bf9042d971
Shawn P. Serbin <sserbin@bnl.gov>
2019-05-31 17:53:55 -0400

    Repair

--- examples/run_example_libradtran.sh
+++ examples/run_example_libradtran.sh
@@ -5,4 +5,4 @@ python3 /isofit/utils/surfmodel.py configs/ang20171108t184227_surface.json
 
 # Run experiments 
 python3 /isofit/isofit/isofit.py --level DEBUG configs/ang20171108t173546_horse-libradtran.json 
-#python3 /isofit/isofit/isofit.py --level DEBUG configs/ang20171108t184227_beckmanlawn-libradtran.json 
+python3 /isofit/isofit/isofit.py --level DEBUG configs/ang20171108t184227_beckmanlawn-libradtran.json 
